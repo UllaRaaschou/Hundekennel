2e1241216a0f1e92f0bbe81b12461cae5a1b4028
Abarbesgaard <Abarbesgaard@gmail.com>
2023-10-30 13:31:47 +0100

    Added a list and how to see a specific dog, but it's all with mockdata

--- WPF_Hundekennel/WPF_Hundekennel/ViewModel/CollectionViewModel.cs
+++ WPF_Hundekennel/WPF_Hundekennel/ViewModel/CollectionViewModel.cs
@@ -8,5 +8,27 @@ namespace WPF_Hundekennel.ViewModel
 {
     class CollectionViewModel
     {
+        #region ListeAfPersoner
+        public List<Dog> Dogs { get; set; } 
+            = new List<Dog> 
+            { 
+            new Dog{ DogName = "Buster", DogGender = "Han"},
+            new Dog{ DogName = "Medister", DogGender = "Han"},
+            new Dog{ DogName = "Kaffe", DogGender = "Tæve"},
+            };
+        #endregion
+
+        #region SelectedDog
+        public Dog SelectedDog { get; set; }
+
+        #endregion
+    }
+    // OBS FØLGENDE KLASSE ER EN TEST KLASSE OG IKKE ENDELIG IMPLEMENTERING
+    class Dog
+    {
+        public string DogName { get; set; }
+        public string DogGender { get; set; }
+
+        
     }
 }
